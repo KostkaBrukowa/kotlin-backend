59cb492fbece37bc53e70ddc3c43a8378b03e973
kostkabrukowa <jiirra@gmail.com>
2020-01-18 14:59:24 +0100

    Set up integration tests

---
+++ src/test/groovy/com/example/graphql/domain/user/UserServiceTests.groovy
@@ -0,0 +1,35 @@
+package com.example.graphql.domain.user
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class UserServiceTests extends Specification {
+    UserRepository userRepository = Stub()
+    UserService userService = new UserService(userRepository)
+
+    def "service should response with correct user"() {
+        given:
+        userRepository.getUserById("1") >> UserTestBuilder.defaultUser([
+                email: "test@email.com",
+                id: "1"
+        ])
+
+        when:
+        def result = userService.getUserById("1")
+
+        then:
+        result.id == "1"
+        result.email == "test@email.com"
+    }
+
+    def "service should respond with null when user is not found in database"() {
+        given:
+        userRepository.getUserById("1") >> null
+
+        when:
+        def result = userService.getUserById("1")
+
+        then:
+        result == null
+    }
+}
---
+++ src/test/groovy/intergration/domain/user/UserMutationTest.groovy
@@ -0,0 +1,40 @@
+package intergration.domain.user
+
+import com.auth0.jwt.JWT
+import intergration.BaseIntegrationSpec
+
+class UserMutationTest extends BaseIntegrationSpec {
+
+    def decodeJWT(String jwt) {
+        return JWT.decode(jwt).subject
+    }
+
+    def "test"() {
+        given:
+        def signUpMutation = 'signUp(input: {email: "a@gmail.com", password: "fdak"})'
+
+        and:
+        def signUpQuery = { String id ->
+            """
+            getUser(id: "${id}"){
+              id
+              name
+              partyRequests {
+                id
+              }
+            }
+        """
+        }
+
+        when:
+        def newUserJWTToken = postMutation(signUpMutation, "signUp")
+
+        and:
+        def getUserResponse = postQuery(signUpQuery(decodeJWT(newUserJWTToken)), "getUser")
+
+        then:
+        getUserResponse.id == "2"
+        getUserResponse.name == null
+        getUserResponse.partyRequests.size() == 0
+    }
+}
