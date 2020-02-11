4b9047291bf8bba5b66acf55bc18743b5abd47e5
kostkabrukowa <jiirra@gmail.com>
2020-02-11 17:54:48 +0100

    CR fixes

--- src/main/kotlin/com/example/graphql/adapters/pgsql/payment/PgSqlPaymentRepository.kt
+++ src/main/kotlin/com/example/graphql/adapters/pgsql/payment/PgSqlPaymentRepository.kt
@@ -1,6 +1,8 @@
 package com.example.graphql.adapters.pgsql.payment
 
 import com.example.graphql.domain.payment.PaymentRepository
+import org.springframework.stereotype.Component
 
+@Component
 class PgSqlPaymentRepository: PaymentRepository {
 }
